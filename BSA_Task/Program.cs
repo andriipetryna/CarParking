28f7560b8782402b0444946333f71d69d9ebb6f4
Andriy Petryna <and.mmars@gmail.com>
2018-05-13 10:44:19 +0300

    Created project (fixes #2)

---
+++ BSA_Task/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BSA_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
